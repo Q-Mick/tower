1eb812c3c50f653095ded2d9a5a2424543ecacbd
Q-Mick <quentinmick@protonmail.com>
2023-06-06 22:43:44 -0600

    tower init

---
+++ tower.client/src/env.js
@@ -0,0 +1,6 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = 'dev-pv3d0c0phnn17vle.us.auth0.com'
+export const audience = 'Http://sandbox.com'
+export const clientId = 'RrUWI4Z64ZfQvbPQ5PXr0Bz6iuUCgKZo'
